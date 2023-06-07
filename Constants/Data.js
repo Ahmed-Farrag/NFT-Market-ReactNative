667e7f5cc825e48869d62cdf2a0ad40847dcaccd
ahmed-farrag <ahmedfaarrag@gmail.com>
2023-06-07 20:06:15 +0300

    first commit

---
+++ Constants/Data.js
@@ -0,0 +1,38 @@
+import nft1 from "../assets/images/nft01.jpg";
+import nft2 from "../assets/images/nft02.jpg";
+import nft3 from "../assets/images/nft03.jpg";
+import nft4 from "../assets/images/nft04.jpg";
+import nft5 from "../assets/images/nft05.jpg";
+import nft6 from "../assets/images/nft06.jpg";
+import nft7 from "../assets/images/nft07.jpg";
+import nft8 from "../assets/images/nft08.jpg";
+import nft9 from "../assets/images/nft09.jpg";
+import nft10 from "../assets/images/nft10.jpg";
+import nft11 from "../assets/images/nft11.jpg";
+import nft12 from "../assets/images/nft12.jpg";
+import avatar1 from "../assets/images/avatars/avatar01.jpg";
+import avatar2 from "../assets/images/avatars/avatar02.jpg";
+import avatar3 from "../assets/images/avatars/avatar03.jpg";
+
+export const DATA = [
+  {
+    id: "1",
+    name: "Cryptopunk #240",
+    creator: "Cryptopunk",
+    price: 4.25,
+    views: "1.5K",
+    comments: 12,
+    image: nft1,
+    date: "21.04.2022",
+    topBid: "",
+    address: "0x245cf...adew",
+    tokenId: 10216,
+    tokenSt: "ERC-721",
+    blockchain: "Ethereum",
+    avatars: [
+      { id: "1", image: avatar1 },
+      { id: "2", image: avatar2 },
+      { id: "3", image: avatar3 },
+    ],
+  },
+];
